785e72cc2e704d295c9f0afe91c5cc6b7e385173
Keshaw Thakur <keshavthakur676@gmail.com>
2020-12-24 21:41:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+let items =["Coding",
+    "Gym","PUBG"];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+
+let today = new Date();
+
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+let day = today.toLocaleDateString("en-US", options);
+
+// to add multiple html we use res.write();
+
+
+res.render("list", 
+{
+    listTitle: day,
+    newListItems: items
+});
+});
+
+app.post("/", function(req,res){
+    let item = req.body.newItem;
+    if(req.body.list  === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req,res){
+    res.render("about");
+})
+
+app.listen(5001, function(){
+    console.log("Server started at port 5001.");
+});
